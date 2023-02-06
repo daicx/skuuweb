f709a400a9452150a9a3d8ff75754b580e27fa6c
daichangxia <changxia.dai@theone.art>
2023-02-07 02:06:19 +0800

    js切分

--- main.dart.js_15.part.js
+++
@@ -1,6 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[32]
-B=c[0]
-var z=a.updateTypes([]);(function constants(){A.mC=new B.dR(1,-1)})()}
-$__dart_deferred_initializers__["MltI1ZYVxr6nc0L/ugBSuqGPqVY="] = $__dart_deferred_initializers__.current
--- main.dart.js_16.part.js
+++
@@ -1,6 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[24]
-B=c[0]
-var z=a.updateTypes([]);(function constants(){A.ik=new B.bN(58737,!0)})()}
-$__dart_deferred_initializers__["qeoNp3p/TP2VIR6Zi7HsDAV73wU="] = $__dart_deferred_initializers__.current
